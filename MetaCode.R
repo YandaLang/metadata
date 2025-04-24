ebc3b91c589429c37af1db189d64392e0aa502d9
YandaLang <62954753+YandaLang@users.noreply.github.com>
2025-04-24 16:25:34 -0400

    MetaCode.R

--- MetaCode.R
+++ MetaCode.R
@@ -6,10 +6,17 @@ url = "https://raw.githubusercontent.com/YandaLang/metadata/refs/heads/main/data
 dat = read.csv(url)
 View(dat)
 
-# fixed and random effect model
-mod = metacont(n.e=n_treat,mean.e=mean_treat,sd.e=std_treat,n.c=n_placebo,mean.c=mean_placebo,sd.c=std_placebo,data=dat,studlab=paste(author,year),sm="MD")
-summary(mod)
+# one sample fixed and random effect model
+mod1 = metamean(n=n_treat,mean=mean_treat,sd=std_treat,data=dat,studlab=paste(author,year))
+summary(mod1)
 
 # forest plot
-forest(mod)
+forest(mod1)
+
+# two sample fixed and random effect model
+mod2 = metacont(n.e=n_treat,mean.e=mean_treat,sd.e=std_treat,n.c=n_placebo,mean.c=mean_placebo,sd.c=std_placebo,data=dat,studlab=paste(author,year),sm="MD")
+summary(mod2)
+
+# forest plot
+forest(mod2)
 
